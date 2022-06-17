07389d4292315ef5c11a77471306d63df958a3af
Kdohyoung <asd62126310@gmail.com>
2022-06-17 11:05:11 +0900

    6-17

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+
+# API 가 있어야 한다.
+@app.route('/',methods=['GET'])
+def hellow_world():
+    return 'Hellow World hihihi'
+
+
+if __name__ == '__main__':
+    app.run()
